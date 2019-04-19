ee3cb076e6663fcb6d863500620aa8e00faf549a
sahadatpro <sahadatsays@gmail.com>
2019-04-20 00:28:20 +0600

    working mission image and mission text

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
        $(".nav-btn").toggleClass("change");
     });
 
-    // Scroll
+    // Scroll for nav
     $(window).scroll(function(){
         var pos = $(this).scrollTop();
         if(pos >= 200) {
@@ -12,4 +12,17 @@ $(document).ready(function(){
             $(".navbar-menu").removeClass("custom-navbar");
         }
     });
+
+    // Scrol for mission animation
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position >= 810) {
+            $('.mission-img').addClass('from-left');
+            $('.mission-text').addClass('from-right');
+        } else {
+            $('.mission-img').removeClass('from-left');
+            $('.mission-text').removeClass('from-right');
+        }
+    });
+
 });
